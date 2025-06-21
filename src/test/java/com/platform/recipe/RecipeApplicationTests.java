9ef164ea4c2585f06451c5ecff5288c2a069bf74
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-06-21 10:34:27 -0300

    [recipe] feat: first commit

---
+++ src/test/java/com/platform/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
