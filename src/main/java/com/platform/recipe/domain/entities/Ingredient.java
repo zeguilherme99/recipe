d7ed4b561fe5eb24fdb708c12374e09775a38791
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-06-21 18:34:40 -0300

    [recipe] chore: adjusting recipe and ingredient entities

--- src/main/java/com/platform/recipe/domain/entities/Ingredient.java
+++ src/main/java/com/platform/recipe/domain/entities/Ingredient.java
@@ -2,30 +2,34 @@ package com.platform.recipe.domain.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "ingredient")
+@Getter
+@Setter
 public class Ingredient {
 
   @Id
-  @Column(name = "id", unique = true, updatable = false, nullable = false)
-  private UUID id;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", unique = true, updatable = false)
+  private Long id;
 
   @Column(name = "name", nullable = false)
   private String name;
 
   @Column(name = "quantity", nullable = false)
-  private Integer quantity;
+  private int quantity;
 
   @Column(name = "unit", nullable = false)
   private String unit;
-
-  private boolean isOptional;
 
   @ManyToOne
   @JoinColumn(name = "recipe_id")
--- src/main/java/com/platform/recipe/domain/entities/Recipe.java
+++ src/main/java/com/platform/recipe/domain/entities/Recipe.java
@@ -3,21 +3,28 @@ package com.platform.recipe.domain.entities;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name = "recipe")
+@Getter
+@Setter
 public class Recipe {
 
   @Id
-  @Column(name = "id", unique = true, updatable = false, nullable = false)
-  private UUID id;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   @Column(name = "title", nullable = false)
   private String title;
@@ -25,11 +32,20 @@ public class Recipe {
   @Column(name = "description", nullable = false)
   private String description;
 
-  @Lob
-  @Column(name = "instructions", nullable = false)
+  @Column(name = "is_vegetarian", nullable = false)
+  private boolean vegetarian;
+
+  @Column(name = "instructions", columnDefinition = "TEXT", nullable = false)
   private String instructions;
 
   @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<Ingredient> ingredients = new ArrayList<>();
 
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false)
+  Timestamp createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at", nullable = false)
+  Timestamp updatedAt;
 }
